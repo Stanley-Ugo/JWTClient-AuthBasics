581cea548df048308d2c3396c576063b815e2a87
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-05-08 06:14:33 +0100

    Authorizing Secret actions

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,7 @@ namespace JWTClient.Controllers
             return View();
         }
 
+        [Authorize]
         public IActionResult Secret()
         {
             return View();
